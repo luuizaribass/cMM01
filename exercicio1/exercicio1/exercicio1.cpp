6f4989861bd0c9bc35c28c0885099af6e1103a90
Maria Luiza Ribas <luuiza.ribass@gmail.com>
2024-06-13 16:31:50 -0300

    .
    .

---
+++ exercicio1/exercicio1/exercicio1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int numero1 = 300;
+	int numero2 = 150;
+	double numero3 = 499.99;
+	double soma = numero1 + numero2 + numero3;
+	cout << soma;
+
+	return 0;
+}
+
+using namespace std;
+int main() {
+	int numero1 = 35;
+	int numero2 = 8;
+	int mult = numero1 * numero2;
+	cout << mult;
+
+	return 0;
+}
+
+using namespace std;
+int main() {
+	int numero1 = 27;
+	int numero2 = 16;
+	int sub = numero1 - numero2;
+	cout << sub;
+
+	return 0;
+}
+
+using namespace std;
+int main() {
+	int numero1 = 300;
+	int numero2 = 20;
+	int div = numero1 / numero2;
+	cout << div;
+
+	return 0;
+}
+
+using namespace std;
+int main() {
+	int numero1 = 120;
+	int numero2 = 80;
+	int numero3 = 30;
+	int numero4 = 50;
+	
+	int total_camisetas = numero1 - numero2 + numero4;
+
+	float faturamento = numero2 * numero3;
+
+	cout << "Total de camisetas agora: " << total_camisetas;
+	cout << "\n";
+	cout << "Faturamento total: R$ " << faturamento;
+
+
+	return 0;
+}
